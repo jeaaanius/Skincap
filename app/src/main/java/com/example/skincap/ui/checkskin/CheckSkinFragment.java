d2e477a2825a1884a5d3b6290c3a4f692a93c18a
strongforce1 <fporquillo18@gmail.com>
2021-09-12 02:13:47 +0800

    Add CheckSkin components

---
+++ app/src/main/java/com/example/skincap/ui/checkskin/CheckSkinFragment.java
@@ -0,0 +1,12 @@
+package com.example.skincap.ui.checkskin;
+
+import com.example.skincap.R;
+import com.example.skincap.databinding.FragmentCheckSkinBinding;
+import com.example.skincap.ui.base.BaseFragment;
+
+public class CheckSkinFragment extends BaseFragment<FragmentCheckSkinBinding> {
+
+    public CheckSkinFragment() {
+        super(R.layout.fragment_check_skin);
+    }
+}
