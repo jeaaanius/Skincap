88c3296a89293b15f38593165ff157942df2e3c5
jeaaanius <limborock15@gmail.com>
2021-10-04 14:52:02 +0800

    Add Library Fragment

---
+++ app/src/main/java/com/example/skincap/ui/library/LibraryFragment.java
@@ -0,0 +1,12 @@
+package com.example.skincap.ui.library;
+
+import com.example.skincap.R;
+import com.example.skincap.databinding.FragmentLibraryBinding;
+import com.example.skincap.ui.base.BaseFragment;
+
+public class LibraryFragment extends BaseFragment<FragmentLibraryBinding> {
+
+    public LibraryFragment() {
+        super(R.layout.fragment_library);
+    }
+}
