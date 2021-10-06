6b45e6b7e107bf5f0d7968b3547fa7b1a9721567
jeaaanius <limborock15@gmail.com>
2021-10-07 05:37:37 +0800

    Add SettingsFragment

---
+++ app/src/main/java/com/example/skincap/ui/settings/SettingsFragment.java
@@ -0,0 +1,12 @@
+package com.example.skincap.ui.settings;
+
+import com.example.skincap.R;
+import com.example.skincap.databinding.FragmentSettingsBinding;
+import com.example.skincap.ui.base.BaseFragment;
+
+public class SettingsFragment extends BaseFragment<FragmentSettingsBinding>{
+
+    public SettingsFragment() {
+        super(R.layout.fragment_settings);
+    }
+}
