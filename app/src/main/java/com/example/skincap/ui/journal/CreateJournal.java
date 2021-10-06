9d649d8aac12616aa5bce58d866f3c10aa04b233
jeaaanius <limborock15@gmail.com>
2021-10-07 05:37:13 +0800

    Create CreateJournal.java

---
+++ app/src/main/java/com/example/skincap/ui/journal/CreateJournal.java
@@ -0,0 +1,17 @@
+package com.example.skincap.ui.journal;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.skincap.R;
+
+public class CreateJournal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_journal);
+    }
+}
