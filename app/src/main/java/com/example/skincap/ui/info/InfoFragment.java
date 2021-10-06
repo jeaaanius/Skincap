f74f3656f68f1ae0e7d508aab333976cb799132c
jeaaanius <limborock15@gmail.com>
2021-10-07 05:36:47 +0800

    Add InfoFragment

---
+++ app/src/main/java/com/example/skincap/ui/info/InfoFragment.java
@@ -0,0 +1,13 @@
+package com.example.skincap.ui.info;
+
+import com.example.skincap.R;
+import com.example.skincap.databinding.FragmentInfoBinding;
+import com.example.skincap.ui.base.BaseFragment;
+
+public class InfoFragment extends BaseFragment<FragmentInfoBinding> {
+
+    public InfoFragment()  {
+        super(R.layout.fragment_info);
+    }
+
+}
