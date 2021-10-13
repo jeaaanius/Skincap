b2e414af9739f08a93b23a78dd70a6994c59e747
4s <fporquillo18@gmail.com>
2021-10-13 20:45:41 +0800

    Fix library component

--- app/src/main/java/com/example/skincap/ui/library/LibraryAdapter.java
+++ app/src/main/java/com/example/skincap/ui/library/LibraryAdapter.java
@@ -10,70 +10,57 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.skincap.R;
+import com.example.skincap.databinding.LibraryRowBinding;
 
-import java.util.List;
-
-public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.LibraryVH> {
-
-    private static final String TAG = "LibraryAdapter";
-    List<Library> libraryList;
-
-    public LibraryAdapter(List<Library> libraryList) {
-        this.libraryList = libraryList;
-    }
+public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.LibraryViewHolder> {
 
     @NonNull
     @Override
-    public LibraryVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.library_row, parent, false);
-        return new LibraryVH(view);
+    public LibraryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        LibraryRowBinding binding = LibraryRowBinding.inflate(inflater, parent, false);
+        return new LibraryViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull LibraryVH holder, int position) {
-
-        Library library = libraryList.get(position);
-        holder.skinIssue.setText(library.getSkinIssue());
-        holder.definition_desc.setText(library.getDefinition());
-        holder.causes_desc.setText(library.getCauses());
-        holder.ingred_desc.setText(library.getIngredient());
-
-        boolean isExpanded = libraryList.get(position).isExpanded();
-        holder.expandableLayout.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
-
+    public void onBindViewHolder(@NonNull LibraryViewHolder holder, int position) {
+        Library library = LibraryDataSource.skinIssueList.get(position);
+        holder.bindData(library);
     }
 
     @Override
     public int getItemCount() {
-        return libraryList.size();
+        return LibraryDataSource.skinIssueList.size();
     }
 
-    class LibraryVH extends RecyclerView.ViewHolder {
+    protected class LibraryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private static final String TAG = "LibraryVH";
+        private final LibraryRowBinding binding;
 
-        ConstraintLayout expandableLayout;
-        TextView skinIssue, definition_desc, causes_desc, ingred_desc;
+        public LibraryViewHolder(@NonNull final LibraryRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
 
-        public LibraryVH(@NonNull final View itemView) {
-            super(itemView);
+            binding.skinIssue.setOnClickListener(this);
+            binding.expandableLayout.setOnClickListener(this);
+        }
 
-            skinIssue = itemView.findViewById(R.id.skinIssue);
-            definition_desc = itemView.findViewById(R.id.definition_desc);
-            causes_desc = itemView.findViewById(R.id.causes_desc);
-            ingred_desc = itemView.findViewById(R.id.ingred_desc);
-            expandableLayout = itemView.findViewById(R.id.expandableLayout);
+        public void bindData(final Library library) {
+            binding.skinIssue.setText(library.getSkinIssue());
+            binding.definitionDesc.setText(library.getDefinition());
+            binding.causesDesc.setText(library.getCauses());
+            binding.ingredDesc.setText(library.getIngredient());
 
-            skinIssue.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+            binding.expandableLayout.setVisibility(library.isExpanded() ? View.VISIBLE : View.GONE);
+        }
 
-                    Library library = libraryList.get(getAdapterPosition());
-                    library.setExpanded(!library.isExpanded());
-                    notifyItemChanged(getAdapterPosition());
-
-                }
-            });
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.skinIssue) {
+                Library library = LibraryDataSource.skinIssueList.get(getAdapterPosition());
+                library.setExpanded(!library.isExpanded());
+                notifyItemChanged(getAdapterPosition());
+            }
         }
     }
 }
