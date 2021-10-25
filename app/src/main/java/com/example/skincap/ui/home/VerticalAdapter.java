6b072e4f644af8b0e88e2c02aabaf5d36beef109
jeaaanius <limborock15@gmail.com>
2021-10-25 08:03:49 +0800

    fixed fetch images from url

--- app/src/main/java/com/example/skincap/ui/home/VerticalAdapter.java
+++ app/src/main/java/com/example/skincap/ui/home/VerticalAdapter.java
@@ -100,9 +100,9 @@ public class VerticalAdapter extends RecyclerView.Adapter<VerticalAdapter.MyView
     private List<ChildModel> getSkinTypesItems() {
         return new ArrayList<ChildModel>() {
             {
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Oily"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Dry"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Balanced"));
+                add(new ChildModel("https://photojoy.jp/magazine/wp-content/uploads/2021/05/shutterstock_1454058797-min.jpg", "Oily"));
+                add(new ChildModel("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/woman-with-atopic-dermatitis-royalty-free-image-900018018-1562167997.jpg", "Dry"));
+                add(new ChildModel("https://luxurykitchenspace.com/wp-content/uploads/2021/02/qZX41m7CGNf7aUecy667PjzAVwoLpP-600x410.jpeg", "Balanced"));
             }
         };
     }
@@ -110,13 +110,13 @@ public class VerticalAdapter extends RecyclerView.Adapter<VerticalAdapter.MyView
     private List<ChildModel> getSkinIssues() {
         return new ArrayList<ChildModel>() {
             {
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Acne Papule"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Sunspots"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Whiteheads"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Blackheads"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Fungal Acne"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Perioral Dermatitis"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Milia"));
+                add(new ChildModel("https://www.parvisdesgentils.fr/wp-content/uploads/2019/06/turmeric-turmeric-which-660x400.jpg", "Acne Papule"));
+                add(new ChildModel("https://www.caraprofesor.com/wp-content/uploads/2018/01/penyebab-flek-hitam-di-wajah.jpg", "Sunspots"));
+                add(new ChildModel("https://ovalin.vn/wp-content/uploads/2020/03/mun-cam.jpg", "Whiteheads"));
+                add(new ChildModel("https://4.bp.blogspot.com/-QxdZBKd9G5c/V17zttcR4NI/AAAAAAAABy4/zuJ5cwgym7cM5dLKhH_VwZmtz2uqp3ApQCK4B/s320/black.jpg", "Blackheads"));
+                add(new ChildModel("https://envisionacnecenter.com/wp-content/uploads/2015/06/Folliculitis-300x197.jpg", "Fungal Acne"));
+                add(new ChildModel("https://www.newtonwellesleyderm.com/sbtemplates/sbcommon/images/aad/pamphlets/45-1.jpg", "Perioral Dermatitis"));
+                add(new ChildModel("https://mrdoctor.org/wp-content/uploads/2011/11/white-bump-on-eyelid-pictures.jpg", "Milia"));
             }
         };
     }
@@ -124,11 +124,11 @@ public class VerticalAdapter extends RecyclerView.Adapter<VerticalAdapter.MyView
     private List<ChildModel> getIngredientsItems() {
         return new ArrayList<ChildModel>() {
             {
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Green Tea"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Salicylic acid"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Tea Tree"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Vitamin C"));
-                add(new ChildModel("https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/wbibf5kz0gdidq0b_1614234544.jpeg", "Vitamin E"));
+                add(new ChildModel("https://5.imimg.com/data5/YT/HA/MY-44441454/green-tea-extract-300-1000x1000.jpg", "Green Tea"));
+                add(new ChildModel("https://ryalix.com/wp-content/uploads/2021/06/9-Best-Skin-Masks-For-Your-Skin-Type-5-1024x683.jpg", "Salicylic acid"));
+                add(new ChildModel("https://www.farmasicolourcosmetics.com/wp-content/uploads/2021/10/tea-tree-essential-oil-500x500-1.jpeg", "Tea Tree"));
+                add(new ChildModel("https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/04/Vitamin_C_1296x728-header.jpg", "Vitamin C"));
+                add(new ChildModel("https://cdn.shopify.com/s/files/1/1944/0163/articles/vitamin-e-rich-food_1140x.jpg?v=1546473551", "Vitamin E"));
             }
         };
 
