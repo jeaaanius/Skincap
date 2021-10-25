3e84c61770089104dd753e5d4702303a1f74049a
jeaaanius <limborock15@gmail.com>
2021-10-25 08:04:10 +0800

    added bullets in each skin care ingredient

--- app/src/main/java/com/example/skincap/ui/library/LibraryDataSource.java
+++ app/src/main/java/com/example/skincap/ui/library/LibraryDataSource.java
@@ -12,9 +12,9 @@ public class LibraryDataSource {
                             "bacteria, excess oil production, excess activity of androgens (male sex hormones)\n" +
                             "Acne can also be triggered or aggravated by:\n" +
                             "stress, diet (such as consuming too much sugar)",
-                    "@ Benzoyl peroxide\n" +
+                    "► Benzoyl peroxide\n" +
                             "is the best single ingredient (antimicrobial) to fight both acne bacteria and yeast.\n\n" +
-                            "@ Salicylic acid\n" +
+                            "► Salicylic acid\n" +
                             "is a keratolytic and has anti-inflammatory, antibacterial, and antifungal properties."));
             add(new Library("Sunspots",
                     "Flat brown spots appear on parts of your skin that have been exposed to the sun.",
@@ -23,24 +23,24 @@ public class LibraryDataSource {
                             "can result in the blemishes forming. This exposure causes your " +
                             "skin to increase its production of melanin, which is the pigment " +
                             "that gives skin its color.",
-                    "@ Licorice extract\n" +
+                    "► Licorice extract\n" +
                             "Some of the active ingredients in licorice extract may help lighten " +
                             "sunspots and other skin discoloration aggravated by sun exposure.\n\n" +
-                            "@ Vitamin C\n" +
+                            "► Vitamin C\n" +
                             "Topical L-ascorbic acid protects your skin from UVA and UVB rays, " +
                             "promotes collagen production, and has been found to be effective in " +
                             "lightening dark spots.\n\n" +
-                            "@ Vitamin E\n" +
+                            "► Vitamin E\n" +
                             "Applying vitamin E oil provides even more benefits for your skin against " +
                             "sun damage and may help lighten sunspots.\n\n" +
-                            "@ Azelaic acid\n" +
+                            "► Azelaic acid\n" +
                             "Azelaic acid treats sunspots and melasma because it blocks the production " +
                             "of \"abnormal pigmentation\".\n\n" +
-                            "@ Mulberry\n" +
+                            "► Mulberry\n" +
                             "Mulberry Extract is known for its natural skin brightening properties " +
                             "and the ability to help fade away dark spots and pigmentation caused by " +
                             "ageing and exposure to the sun.\n\n" +
-                            "@ Niacinamide\n" +
+                            "► Niacinamide\n" +
                             "can be used to treat a wide range of skin issues, including acne, rosacea, " +
                             "hyperpigmentation and wrinkles. Additionally, it provides plenty of benefits " +
                             "for the skin, helping to increase collagen production, reduce moisture loss, " +
@@ -49,68 +49,68 @@ public class LibraryDataSource {
             add(new Library("Whiteheads",
                     "Little, raised lumps on the skin that are white in color.",
                     "When dead skin cells, oil, and bacteria become stuck within one of your pores.",
-                    "@ Tea tree\n" +
+                    "► Tea tree\n" +
                             "touted as a natural anti-inflammatory. It may also have antimicrobial benefits. " +
                             "These effects mean tea tree oil could help clear up whiteheads. (NOT OIL)\n\n" +
-                            "@ Green Tea\n" +
+                            "► Green Tea\n" +
                             "Some of the compounds in green tea have antimicrobial and anti-inflammatory " +
                             "properties. Additionally, it appears to decrease sebum production. " +
                             "Studies looking at its use in mild to moderate acne are promising."));
             add(new Library("Blackheads",
                     "Small lumps on the skin caused by clogged hair follicles.",
                     "",
-                    "@ Azelaic acid\n" +
+                    "► Azelaic acid\n" +
                             "often used as an alternative treatment for acne if the side effects of " +
                             "benzoyl peroxide or topical retinoids are particularly irritating or painful." +
                             "Azelaic acid works by getting rid of dead skin and killing bacteria."));
             add(new Library("Fungal Acne",
                     "Infection in the hair follicles of your skin.",
                     "",
-                    "@ Itraconazole\n" +
+                    "► Itraconazole\n" +
                             "Many investigators have studied the efficacy of itraconazole, as this " +
                             "antifungal is excreted in high concentrations in sebum." +
                             "Itraconazole is a broad-spectrum triazole, which is highly lipophilic " +
                             "and keratophilic with good oral absorption and extensive tissue distribution.\n\n" +
-                            "@ Azelaic Acid\n" +
+                            "► Azelaic Acid\n" +
                             "This natural ingredient and commonly used to treat fungal acne as well " +
                             "as rosacea also has fungal acne properties." +
                             "It may help by regulating the fatty acid content in your skin, a common " +
                             "mechanism used by antifungals. As the yeast or fungi survive on the " +
                             "fatty acids and oils in the skin, reducing the free fatty acid content " +
                             "may help to reduce the yeast population.\n\n" +
-                            "@ Tea Tree\n" +
+                            "► Tea Tree\n" +
                             "Derived from the Melaleuca alternifolia plant, it is a well-known antimicrobial." +
                             "In vitro studies show that it may be effective in inhibiting several " +
                             "species of fungi and we know there are many."));
             add(new Library("Perioral Dermatitis",
                     "A rash involving the skin around the mouth that is inflamed.",
                     "",
-                    "@ Vitamin B3 (INCI: Niacinamide)\n" +
+                    "► Vitamin B3 (INCI: Niacinamide)\n" +
                             "In the case of acne vulgaris where similar germ colonization occurs " +
                             "as in perioral dermatitis, vitamin B3 also has an anti-inflammatory " +
                             "effect which is comparable to clindamycin.\n\n" +
-                            "@ Provitamin B5 (INCI: D-panthenol)\n" +
+                            "► Provitamin B5 (INCI: D-panthenol)\n" +
                             "the pre-stage of pantothenic acid improves the skin hydration." +
                             "It improves the cell formation and epithelialization after skin lesions " +
                             "and inhibits the itching.\n\n" +
-                            "@ Green tea (camellia leaf extract)\n" +
+                            "► Green tea (camellia leaf extract)\n" +
                             "Calms and helps regulate sebum production to promote a balanced, clear complexion."));
             add(new Library("Milia",
                     "Typically develops on the nose and cheeks as a little white lump.",
                     "",
-                    "@ Curettage\n" +
+                    "► Curettage\n" +
                             "*Note: not an ingredient, but a dermatological procedure*\n" +
                             "With this procedure, the dermatologist scrapes off the milia then seals " +
                             "the skin with a hot wire."));
             add(new Library("Oily",
                     "The result of the overproduction of sebum from sebaceous glands.",
                     "Genetics, hormone changes, or even stress may increase sebum production.",
-                    "@ Beta-Hydroxy Acids (BHAs)\n" +
+                    "► Beta-Hydroxy Acids (BHAs)\n" +
                             "BHAs are oil-soluble acids that plunge deep into pores to target oil " +
                             "glands and decrease oil secretion.\n\n" +
-                            "@ Niacinamide\n" +
+                            "► Niacinamide\n" +
                             "Lauded its anti-inflammatory, soothing, brightening, and skin tone-evening benefits\n\n" +
-                            "@ Glycolic Acid\n" +
+                            "► Glycolic Acid\n" +
                             "An AHA that minimizes pores and speeds up cell turnover " +
                             "for fresher, clearer, younger-looking skin."));
             add(new Library("Dry",
@@ -118,20 +118,20 @@ public class LibraryDataSource {
                     "Exposure to dry weather conditions, hot water, and certain chemicals " +
                             "can cause your skin to dry out. Dry skin can also result from " +
                             "underlying medical conditions.",
-                    "@ Alpha-hydroxy acids (AHAs)\n" +
+                    "► Alpha-hydroxy acids (AHAs)\n" +
                             "AHAs are water-soluble acids derived from fruits or sugar cane that " +
                             "exfoliate dull, dry skin.\n\n" +
-                            "@ Ceramides \n" +
+                            "► Ceramides \n" +
                             "This type of naturally-occurring fat holds skin cells together to help " +
                             "form a waterproof seal and healthy skin barrier that keeps skin hydrated.\n\n" +
-                            "@ Cica\n" +
+                            "► Cica\n" +
                             "Centella asiatica (a.k.a. cica) is an herb rich in anti-inflammatories " +
                             "that calms redness and irritation, regulates collagen synthesis, and " +
                             "protects against environmental damage.\n\n" +
-                            "@ Humectants\n" +
+                            "► Humectants\n" +
                             "water-loving ingredients that draw moisture into skin to help increase " +
                             "moisture content over time.\n\n" +
-                            "@ Hyaluronic Acid (HA)\n" +
+                            "► Hyaluronic Acid (HA)\n" +
                             "this gentle humectant attracts up to 1,000 times its weight in water " +
                             "to hydrate, smooth, and plump dry or wrinkled skin."));
         }
