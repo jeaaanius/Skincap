9fbc64190308f14b589a301383168bda47a439c7
Emmanuel <43689922+peremmm@users.noreply.github.com>
2021-10-28 15:17:24 +0800

    Add gallery module (fixed)
    remove functions since the activity is transferred to ResultActivity

--- app/src/main/java/com/example/skincap/ui/checkskin/CheckSkinFragment.java
+++ app/src/main/java/com/example/skincap/ui/checkskin/CheckSkinFragment.java
@@ -47,130 +47,18 @@ public class CheckSkinFragment extends BaseFragment<FragmentCheckSkinBinding> {
 
     private void onClickCameraButton(View view) {
 
-      startActivity(new Intent(requireActivity(), ResultActivity.class));
+//      startActivity(new Intent(requireActivity(), ResultActivity.class));
+        Intent myIntent = new Intent(requireActivity(), ResultActivity.class);
+        myIntent.putExtra("REQUEST_CODE", 1001);
+        startActivity(myIntent);
     }
 
     private void onClickGalleryButton(View view) {
 
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, GALLERY_REQUEST_CODE);
-
-        //startActivity(new Intent(requireActivity(), ResultActivity.class));
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode != RESULT_OK) {
-            return;
-        }
-
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            //  Camera
-            getCapturedImage(data);
-        }
-
-        if (requestCode == GALLERY_REQUEST_CODE) {
-            //  Gallery
-            getSelectedImage(data);
-        }
-
-    }
-
-    private void getCapturedImage(@Nullable final Intent data) {
-        Bitmap captureImageBitmap = getDataOrNull(data);
-        binding.checkSkinImage.setImageBitmap(captureImageBitmap);
-
-        try {
-            ImageClassifier imageClassifier = new ImageClassifier(requireActivity());
-
-            List<ImageClassifier.Recognition> predictions = imageClassifier.recognizeImage(
-                    captureImageBitmap, 0);
-
-            final List<String> predictionsList = new ArrayList<>();
-
-            for(ImageClassifier.Recognition recog : predictions){
-                predictionsList.add(recog.getName() + " : " + recog.getConfidence());
-            }
-
-            for (String string : predictionsList) {
-                Log.e(null, string);
-            }
-
-            //  ArrayAdapter<String> predictionsAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, predictionsList);
-
-        } catch (IOException e) {
-            Log.e("Image Classifier Error", "ERROR: " + e);
-        }
-        // FOR SKIN ANALYZER
-        try {
-            AnalyzerClassifier imageClassifier = new AnalyzerClassifier(requireActivity());
-            List<AnalyzerClassifier.Recognition> predictions = imageClassifier.recognizeImage(
-                    captureImageBitmap, 0);
-            final List<String> predictionsList = new ArrayList<>();
-            for(AnalyzerClassifier.Recognition recog : predictions){
-                predictionsList.add(recog.getName() + " : " + recog.getConfidence());
-            }
-            for (String string : predictionsList) {
-                Log.e(null, string);
-            }
-
-        } catch (IOException e) {
-            Log.e("Image Classifier Error", "ERROR: " + e);
-        }
-
-    }
-
-    private void getSelectedImage(@Nullable final Intent data) {
-
-        Uri selectedImage = data.getData();
-        try {
-            Bitmap selectedImageBitmap = MediaStore.Images.Media.getBitmap(requireActivity().getContentResolver(),selectedImage);
-            binding.checkSkinImage.setImageBitmap(selectedImageBitmap);
-
-           /* ImageClassifier imageClassifier = new ImageClassifier(requireActivity());
-
-            List<ImageClassifier.Recognition> predictions = imageClassifier.recognizeImage(
-                    selectedImageBitmap, 0);
-
-            final List<String> predictionsList = new ArrayList<>();
-
-            for(ImageClassifier.Recognition recog : predictions){
-                predictionsList.add(recog.getName() + " : " + recog.getConfidence());
-            }
-
-            for (String string : predictionsList) {
-                Log.e(null, string);
-            }*/
-
-        } catch(IOException e){
-            e.printStackTrace();
-            //Log.e("Image Classifier Error", "ERROR: " + e);
-        }
-
-        /*// FOR SKIN ANALYZER
-        try {
-            Bitmap selectedImageBitmap = MediaStore.Images.Media.getBitmap(requireActivity().getContentResolver(),selectedImage);
-            binding.checkSkinImage.setImageBitmap(selectedImageBitmap);
-
-            AnalyzerClassifier imageClassifier = new AnalyzerClassifier(requireActivity());
-            List<AnalyzerClassifier.Recognition> predictions = imageClassifier.recognizeImage(
-                    selectedImageBitmap, 0);
-            final List<String> predictionsList = new ArrayList<>();
-            for(AnalyzerClassifier.Recognition recog : predictions){
-                predictionsList.add(recog.getName() + " : " + recog.getConfidence());
-            }
-            for (String string : predictionsList) {
-                Log.e(null, string);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e("Image Classifier Error", "ERROR: " + e);
-        }*/
-
-        // startActivity(new Intent(getActivity(), ResultActivity.class));
+//        startActivity(new Intent(requireActivity(), ResultActivity.class));
+        Intent myIntent = new Intent(requireActivity(), ResultActivity.class);
+        myIntent.putExtra("REQUEST_CODE", 1002);
+        startActivity(myIntent);
     }
 
     @SuppressWarnings("unchecked")
