37795bcd1f3d4c48221da15eab60c7290f8c0cb8
strongforce1 <fporquillo18@gmail.com>
2021-11-02 13:29:36 +0800

    Update FilePathParser.java

--- app/src/main/java/com/example/skincap/util/FilePathParser.java
+++ app/src/main/java/com/example/skincap/util/FilePathParser.java
@@ -2,11 +2,16 @@ package com.example.skincap.util;
 
 import android.net.Uri;
 
+import androidx.annotation.Nullable;
+
 import java.io.File;
 
 public class FilePathParser {
 
-    public static Uri parseImagePath(String absolutePath) {
+    public static Uri parseImagePath(@Nullable String absolutePath) {
+        if (absolutePath == null) {
+            return null;
+        }
         return Uri.fromFile(new File(absolutePath));
     }
 }
